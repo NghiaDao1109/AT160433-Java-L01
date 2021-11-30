4bfc77c704970ed05b4d2f14deba76d29f0a6968
NghiaDao1109 <85614484+NghiaDao1109@users.noreply.github.com>
2021-11-30 10:43:21 +0700

    Add files via upload

---
+++ BaiTH2Bai2.java
@@ -0,0 +1,10 @@
+package modaujava;
+import java.util.Scanner;
+public class BaiTH2Bai2 {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        int n = input.nextInt();
+        System.out.println("So " + n + " theo dang nhi phan la: " + Integer.toBinaryString(n));
+    }
+    
+}
